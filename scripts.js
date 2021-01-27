0bc8d474daf0dae9b35ce31a43e595d04e2c2ceb
i-alexis <77300748+i-alexis@users.noreply.github.com>
2021-01-27 13:15:58 -0500

    more choices

---
+++ scripts.js
@@ -0,0 +1,52 @@
+function removeClassYeet(elementid){
+    document.getElementById(elementid)
+        .classList.remove("yeet")
+    if(elementid === "goToPark"){
+        document.getElementById("somewhereelse")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    
+    if(elementid === "Ignore"){
+        document.getElementById("dontignore")
+        .style.display="none"
+    }
+    if(elementid === "dontIgnore"){
+        document.getElementById("ignore")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+    if(elementid === "goSomewhereElse"){
+        document.getElementById("park")
+        .style.display="none"
+    }
+}
+
+// let choice1 = document.getElementById("tomorrow")
+// choice1.addEventListener("click", removeClassYeet("maybeTomorrow"))
